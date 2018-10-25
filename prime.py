d0f9be3f7d1ea8a879e4ad6c7184977f81042761
Reijer van der Zande <rz@warpnet.nl>
2018-10-25 16:39:54 +0200

    trial divisions

--- prime.py
+++ prime.py
@@ -15,34 +15,70 @@ class prime(object):
 		elif n > 1:
 			nmax = int(math.sqrt(n)+1)
 			for prime in self.primes:
-				if prime >= nmax:
-					self.primes.append(n)
+				if n in self.primes:
 					return True
-				elif n in self.primes:
+				elif prime >= nmax:
+					self.primes.append(n)
 					return True
 				elif n % prime == 0:
 					return False
 		else:
 			return False
 
-	def nextprime(self):
-		n = 1
+	def nextprime(self, n=1):
 		while True:
-			n += 2
-			if n in self.primes:
-				yield n
-			elif self.isprime(n):
-				yield n
+			n += 1
+			if n > len(self.primes):
+				currentnumber = self.primes[-1]+1
+				while not self.isprime(currentnumber):
+					currentnumber += 1
+			yield self.primes[n-2]
+
+	def giveprimes(self, n):
+		"""returns list up to and including n"""
+		if n > self.primes[-1]:
+			for current in range(self.primes[-1], n+1):
+				self.isprime(current)
+		return [prime for prime in self.primes if prime <= n]
 
 
-if __name__ == '__main__':
+def test():
+	print "check function .isprime(n)"
 	p = prime()
 	listofprimes = [i for i in range(-10, 100) if p.isprime(i)]
 	print listofprimes
+	print
+	print "check currentprimes"
+	g = prime()
+	print "while empty"
 	print p.currentprimes()
-	gen = p.nextprime()
-	for i in range(110):
-		print next(gen)
+	print "fill with primes upto 20"
+	listofprimes = [i for i in range(20) if p.isprime(i)]
 	print p.currentprimes()
+	print
+	print "generator nextprime"
+	h = prime()
+	print "give the first 10 primenumbers"
+	for i in range(10):
+		print next(p.nextprime())
+
+
+
+if __name__ == '__main__':
+	# test()
+	p = prime()
+	for i in range(1, 101):
+		print i, p.giveprimes(i)
+
+	g = prime()
+	gen = g.nextprime()
+	for i in range(1, 101):
+		print i, next(gen)
+
+	gen = g.nextprime(10)
+	for i in range(1, 101):
+		print i+10, next(gen)
+
+
 
 
